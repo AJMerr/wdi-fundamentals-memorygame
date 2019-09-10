981da1456cad4a8968252bf69568ecba71905aea
Austin <austinm9506@gmail.com>
2019-09-09 22:46:13 -0400

    Added conditional statements to the memory card game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,29 @@
 console.log("Up and running!");
 
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
+//Card Tables
 
-console.log("User flipped " + cardThree);
-console.log("user flipped " + cardTwo);
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+//Card 1 - 4 variable
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+// Else if statement to check if two cards are in play
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Oops, try again!");
+	}
+}
+
+
+	console.log( "User has flipped " + cardsInPlay);
